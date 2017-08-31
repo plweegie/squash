16be8d568c3fa429ea746aeb10d4c5e854298950
Jan K Szymanski <jan.kazimierz.szymanski@gmail.com>
2017-09-01 00:57:59 +0100

    Read favorites from Firebase DB

--- app/src/main/java/com/plweegie/android/squash/FaveListFragment.java
+++ app/src/main/java/com/plweegie/android/squash/FaveListFragment.java
@@ -7,12 +7,34 @@ package com.plweegie.android.squash;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.plweegie.android.squash.adapters.FaveAdapter;
+import com.plweegie.android.squash.utils.Repository;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author jan
  */
 public class FaveListFragment extends Fragment {
+    
+    private List<Repository> mFaveRepos;
+    private RecyclerView mRecyclerView;
+    private FaveAdapter mAdapter;
+    
+    private DatabaseReference mDatabase;
     
     public static FaveListFragment newInstance() {
         return new FaveListFragment();
@@ -22,5 +44,44 @@ public class FaveListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.commit_list_fragment, parent, false);
+        
+        mFaveRepos = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference("repositories");
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.commits_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),
+                LinearLayoutManager.VERTICAL));
+        
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot child: snapshot.getChildren()) {
+                    mFaveRepos.add(child.getValue(Repository.class));
+                }
+                if (mAdapter == null) {
+                    mAdapter = new FaveAdapter(getActivity(), mFaveRepos);
+                    mAdapter.setHasStableIds(true);
+                    mRecyclerView.setAdapter(mAdapter);
+                } else {
+                    mAdapter.setContent(mFaveRepos);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+            
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.e("FaveListFragment", error.toException().toString());
+            }
+        });
+        return v;
     }
 }
--- app/src/main/java/com/plweegie/android/squash/adapters/FaveAdapter.java
+++ app/src/main/java/com/plweegie/android/squash/adapters/FaveAdapter.java
@@ -6,12 +6,8 @@
 package com.plweegie.android.squash.adapters;
 
 import android.content.Context;
-import android.graphics.PorterDuff;
-import android.preference.PreferenceManager;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,105 +17,73 @@ import com.plweegie.android.squash.R;
 import com.plweegie.android.squash.utils.Repository;
 import java.util.List;
 
+public class FaveAdapter extends RecyclerView.Adapter<FaveAdapter.FaveHolder> {
 
-public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.RepoHolder> {
-    
-    private List<Repository> mRepos;
+    private List<Repository> mFaveRepos;
     private Context mContext;
-    
-    public RepoAdapter(Context context, List<Repository> repos) {
-        mRepos = repos;
+
+    public FaveAdapter(Context context, List<Repository> repos) {
+        mFaveRepos = repos;
         mContext = context;
     }
 
     @Override
-    public RepoHolder onCreateViewHolder(ViewGroup vg, int i) {
+    public FaveHolder onCreateViewHolder(ViewGroup vg, int i) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        return new RepoHolder(inflater, vg, R.layout.repo_view_holder);
+        return new FaveHolder(inflater, vg, R.layout.repo_view_holder);
     }
 
     @Override
-    public void onBindViewHolder(RepoHolder vh, int i) {
-        Repository repo = mRepos.get(i);
+    public void onBindViewHolder(FaveHolder vh, int i) {
+        Repository repo = mFaveRepos.get(i);
         vh.bind(repo);
     }
 
     @Override
     public int getItemCount() {
-        return mRepos.size();
+        return mFaveRepos.size();
     }
-    
+
     @Override
     public long getItemId(int position) {
         return position;
     }
-    
+
     public void setContent(List<Repository> repos) {
-        mRepos = repos;
+        mFaveRepos = repos;
     }
-    
-    public class RepoHolder extends RecyclerView.ViewHolder implements
-            View.OnClickListener {
-        
+
+    public class FaveHolder extends RecyclerView.ViewHolder {
+
         private Repository mRepo;
         private DatabaseReference mDatabase;
-        
+
         private TextView mNameTextView;
         private TextView mLangTextView;
         private TextView mStarCountTextView;
         private TextView mWatchCountTextView;
         private ImageView mFavoriteImgView;
-        
-        public RepoHolder(LayoutInflater inflater, ViewGroup parent,
+
+        public FaveHolder(LayoutInflater inflater, ViewGroup parent,
                 int layoutResId) {
             super(inflater.inflate(layoutResId, parent, false));
-            itemView.setOnClickListener(this);
             mDatabase = FirebaseDatabase.getInstance().getReference("repositories");
-            
+
             mNameTextView = (TextView) itemView.findViewById(R.id.repo_name_tv);
             mLangTextView = (TextView) itemView.findViewById(R.id.repo_language_tv);
             mStarCountTextView = (TextView) itemView.findViewById(R.id.stars_tv);
             mWatchCountTextView = (TextView) itemView.findViewById(R.id.watchers_tv);
             mFavoriteImgView = (ImageView) itemView.findViewById(R.id.fave_image_view);
         }
-        
+
         public void bind(Repository repo) {
-            
+
             mRepo = repo;
-            
+
             mNameTextView.setText(repo.getName());
             mLangTextView.setText(repo.getLanguage());
             mStarCountTextView.setText(repo.getStargazersCount().toString());
             mWatchCountTextView.setText(repo.getWatchersCount().toString());
-            
-            if (repo.isFavorite()) {
-                mFavoriteImgView.setImageResource(R.drawable.ic_favorite_black_24dp);
-                mFavoriteImgView.setColorFilter(ResourcesCompat
-                        .getColor(mContext.getResources(), R.color.colorAccent, null),
-                        PorterDuff.Mode.SRC_IN);
-            } else {
-                mFavoriteImgView.setImageResource(R.drawable.ic_favorite_border_black_24dp);
-                mFavoriteImgView.setColorFilter(ResourcesCompat
-                        .getColor(mContext.getResources(), R.color.colorInactive, null),
-                        PorterDuff.Mode.SRC_IN);
-            }
-        }
-        
-        @Override
-        public void onClick(View view) {
-            int position = getAdapterPosition();
-            mRepo.setIsFavorite(!mRepo.isFavorite());
-            
-            if (mRepo.isFavorite()) {
-                mDatabase.child(String.valueOf(position))
-                        .setValue(mRepo);
-                PreferenceManager.getDefaultSharedPreferences(mContext)
-                        .edit()
-                        .putBoolean(String.valueOf(position), true)
-                        .apply();
-                notifyItemRemoved(position);
-                mRepos.remove(position);
-            }
         }
     }
 }
