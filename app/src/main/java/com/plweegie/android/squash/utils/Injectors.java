e5de012ecdc0446728c46ed870b76072a2246b7b
Jan Szymanski <jan.kazimierz.szymanski@gmail.com>
2018-02-04 17:29:13 +0000

    Do cleanup

--- app/src/main/java/com/plweegie/android/squash/utils/Injectors.java
+++
@@ -1,9 +0,0 @@
-package com.plweegie.android.squash.utils;
-
-public class Injectors {
-//    public static RepoRepository provideRepository(Context context) {
-//        RepoDatabase db = RepoDatabase.getInstance(context);
-//        AppExecutors executors = AppExecutors.getInstance();
-//        return RepoRepository.getInstance(db.repoDao(), executors);
-//    }
-}
